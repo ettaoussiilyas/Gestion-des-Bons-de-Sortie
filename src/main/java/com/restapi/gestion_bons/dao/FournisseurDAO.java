af74519d1f01eacf760f28708c35832ebfb438a6
ettaoussi <ettaoussiilyas0.1@gmail.com>
2025-10-28 15:49:07 +0100

    creation of class DAO for Entity Fournisseur

---
+++ src/main/java/com/restapi/gestion_bons/dao/FournisseurDAO.java
@@ -0,0 +1,7 @@
+package com.restapi.gestion_bons.dao;
+
+import com.restapi.gestion_bons.entitie.Fournisseur;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FournisseurDAO extends JpaRepository<Fournisseur, Integer> {
+}
