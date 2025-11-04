5e0f48de93b49cbde4a62404e5058fd929455779
ettaoussi <ettaoussiilyas0.1@gmail.com>
2025-11-04 15:22:15 +0100

    Create DTO for Atelier

---
+++ src/main/java/com/restapi/gestion_bons/dto/atelier/AtelierResponseDTO.java
@@ -0,0 +1,12 @@
+package com.restapi.gestion_bons.dto.atelier;
+
+import com.restapi.gestion_bons.entitie.BonDeSortie;
+
+import java.util.List;
+
+public class AtelierResponseDTO {
+
+    private Long id;
+    private String nom;
+
+}
--- src/main/java/com/restapi/gestion_bons/dto/fournisseur/FournisseurCreateDTO.java
+++ src/main/java/com/restapi/gestion_bons/dto/fournisseur/FournisseurCreateDTO.java
@@ -1,16 +1,9 @@
 package com.restapi.gestion_bons.dto.fournisseur;
 
-import com.restapi.gestion_bons.entitie.CommandeFournisseur;
-import jakarta.persistence.*;
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Pattern;
-import jakarta.validation.constraints.Size;
+
 import lombok.*;
 import lombok.experimental.SuperBuilder;
 
-import java.util.ArrayList;
-import java.util.List;
 
 @Data
 @SuperBuilder
