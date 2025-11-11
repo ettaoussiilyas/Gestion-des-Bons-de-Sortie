d74cd6d40bfdc728598f45be68c2d7c3cc9ece96
ettaoussi <ettaoussiilyas0.1@gmail.com>
2025-11-11 11:22:48 +0100

    test : Creation of folder and file for testing exerices

---
+++ src/test/java/com/restapi/gestion_bons/service/commandeFournisseur/CommandeFournisseurServiceTest.java
@@ -0,0 +1,31 @@
+package com.restapi.gestion_bons.service.commandeFournisseur;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+@DisplayName("Commande Fournisseur Service Unit Test")
+class CommandeFournisseurServiceTest {
+
+    @Test
+    void pass(){
+//        pass();
+    }
+
+    @Test
+    void fail(){
+        fail();
+    }
+
+    @Test
+    @DisplayName("Disable from debug")
+    @Disabled
+    void disabled(){
+
+    }
+}
---
+++ src/test/java/com/restapi/gestion_bons/service/examples/ProduitsEtFournisseurs.java
@@ -0,0 +1,12 @@
+package com.restapi.gestion_bons.service.examples;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+@DisplayName("Produits et Fournisseurs Exercices")
+public class ProduitsEtFournisseurs {
+
+
+}
