3f55d6b9d54429beedc69fc795c9258ec3af448b
ettaoussi <ettaoussiilyas0.1@gmail.com>
2025-11-14 17:20:27 +0100

    fetch : adding a JPQL query to get a custized query result

--- src/main/java/com/restapi/gestion_bons/dao/MouvementStockDAO.java
+++ src/main/java/com/restapi/gestion_bons/dao/MouvementStockDAO.java
@@ -2,6 +2,11 @@ package com.restapi.gestion_bons.dao;
 
 import com.restapi.gestion_bons.entitie.MouvementStock;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.time.LocalDateTime;
+import java.util.Date;
 import java.util.List;
 
 public interface MouvementStockDAO extends JpaRepository<MouvementStock, Long> {
@@ -12,4 +17,9 @@ public interface MouvementStockDAO extends JpaRepository<MouvementStock, Long> {
     List<MouvementStock> findByLotIdOrderByDateMouvementDesc(Long lotId);
 
     List<MouvementStock> findByBonDeSortieIdOrderByDateMouvementDesc(Long bonDeSortieId);
+
+
+    @Query("SELECT m FROM MouvementStock m WHERE m.typeMouvement = :type AND m.dateMouvement BETWEEN :start AND :ends")
+    List<MouvementStock> getListDate(@Param("type") String type, @Param("start") LocalDateTime start, @Param("ends") LocalDateTime date);
+
 }
